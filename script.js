81c7374ad35584ec4173c21d931b5e9a548353aa
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2022-11-07 13:31:13 +0530

    ques-array-added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const mainJs = document.querySelector('main');
+const scoreDisplay = document.querySelector('#score-display');
+const quesDisplay = document.querySelector('#ques-display');
 
 // timer elements
 const counterJs = document.querySelector('.counter');
@@ -12,6 +14,8 @@ const startBtn = document.querySelector('.start-btn');
 const exitBtn = document.querySelector('.exit-btn');
 const yesBtn = document.querySelector('.y-btn');
 const noBtn = document.querySelector('.n-btn');
+const openScoreBtn = document.querySelector('.open');
+const closeScoreBtn = document.querySelector('.close');
 
 // timer
 const startTime = 5;
@@ -27,8 +31,28 @@ updateCounter = () => {
         counterJs.innerHTML = `0${minute} : 0${second}`;
     }
 
+    // if(minute === -1){
+    //     counterJs.innerHTML = `00 : 00`;
+    //     clearInterval(updateCounter);
+    //     window.location.replace('./tq.html');
+    // }
+
     totalTime--;
 }
+
+// OPENING SCORE DISPLAY ON SMALLER SCREEN
+openScoreBtn.addEventListener('click', () => {
+    scoreDisplay.style.display = 'flex';
+    quesDisplay.style.display = 'none';
+    openScoreBtn.style.display = 'none';
+    closeScoreBtn.style.display = 'block'
+})
+closeScoreBtn.addEventListener('click', () => {
+    scoreDisplay.style.display = 'none';
+    quesDisplay.style.display = 'flex';
+    openScoreBtn.style.display = 'block';
+    closeScoreBtn.style.display = 'none'
+})
 
 // START MODAl
 window.addEventListener("load", () => {
@@ -56,3 +80,99 @@ noBtn.addEventListener('click', () => {
 yesBtn.addEventListener('click', () => {
     window.location.replace("./tq.html");
 })
+
+// QUESTIONS
+
+const questions = [
+    {
+        flag: 'https://countryflagsapi.com/png/br',
+        answer : [
+            {option: 'Panama', correct: false},
+            {option: 'Argentina', correct: false},
+            {option: 'Brazil', correct: true},
+            {option: 'Bolivia', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/ru',
+        answer : [
+            {option: 'Russia', correct: true},
+            {option: 'France', correct: false},
+            {option: 'Italy', correct: false},
+            {option: 'Greece', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/bt',
+        answer : [
+            {option: 'Nepal', correct: false},
+            {option: 'Bhutan', correct: true},
+            {option: 'Thailand', correct: false},
+            {option: 'Laos', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/png',
+        answer : [
+            {option: 'Panama', correct: false},
+            {option: 'Vietnam', correct: false},
+            {option: 'Papua New Guinea', correct: true},
+            {option: 'North Macedonia', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/vat',
+        answer : [
+            {option: 'Serbia', correct: false},
+            {option: 'Vatican City', correct: true},
+            {option: 'Somalia', correct: false},
+            {option: 'Bolivia', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/sl',
+        answer : [
+            {option: 'Sri Lanka', correct: false},
+            {option: 'Zanzibar', correct: false},
+            {option: 'Sierra Leone', correct: true},
+            {option: 'Tanzania', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/mex',
+        answer : [
+            {option: 'Panama', correct: false},
+            {option: 'Canada', correct: false},
+            {option: 'Slovakia', correct: false},
+            {option: 'Mexico', correct: true},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/pan',
+        answer : [
+            {option: 'Panama', correct: true},
+            {option: 'Czech Republic', correct: false},
+            {option: 'Cambodia', correct: false},
+            {option: 'Bolivia', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/fiji',
+        answer : [
+            {option: 'Panama', correct: false},
+            {option: 'Fiji', correct: true},
+            {option: 'French Polynesia', correct: false},
+            {option: 'Tuvalu', correct: false},
+        ]
+    },
+    {
+        flag: 'https://countryflagsapi.com/png/tuv',
+        answer : [
+            {option: 'British Indian Territory', correct: false},
+            {option: 'Argentina', correct: false},
+            {option: 'Tuvalu', correct: true},
+            {option: 'Spain', correct: false},
+        ]
+    }
+
+]
