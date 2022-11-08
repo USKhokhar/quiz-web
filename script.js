f96c81cbdb076ddabf9a9fe580ac6453c7f8aded
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2022-11-08 16:50:31 +0530

    ques-added

--- script.js
+++ script.js
@@ -1,89 +1,7 @@
-const mainJs = document.querySelector('main');
-const scoreDisplay = document.querySelector('#score-display');
-const quesDisplay = document.querySelector('#ques-display');
-
-// timer elements
-const counterJs = document.querySelector('.counter');
-
-// modals
-const startModal = document.querySelector('.start-modal');
-const exitModal = document.querySelector('.exit-modal');
-
-// buttons
-const startBtn = document.querySelector('.start-btn');
-const exitBtn = document.querySelector('.exit-btn');
-const yesBtn = document.querySelector('.y-btn');
-const noBtn = document.querySelector('.n-btn');
-const openScoreBtn = document.querySelector('.open');
-const closeScoreBtn = document.querySelector('.close');
-
-// timer
-const startTime = 5;
-let totalTime = startTime * 60;
-
-updateCounter = () => {
-    const minute = Math.floor(totalTime / 60);
-    let second = totalTime % 60;
-
-    
-    counterJs.innerHTML = `0${minute} : ${second}`;
-    if(second < 10){
-        counterJs.innerHTML = `0${minute} : 0${second}`;
-    }
-
-    // if(minute === -1){
-    //     counterJs.innerHTML = `00 : 00`;
-    //     clearInterval(updateCounter);
-    //     window.location.replace('./tq.html');
-    // }
-
-    totalTime--;
-}
-
-// OPENING SCORE DISPLAY ON SMALLER SCREEN
-openScoreBtn.addEventListener('click', () => {
-    scoreDisplay.style.display = 'flex';
-    quesDisplay.style.display = 'none';
-    openScoreBtn.style.display = 'none';
-    closeScoreBtn.style.display = 'block'
-})
-closeScoreBtn.addEventListener('click', () => {
-    scoreDisplay.style.display = 'none';
-    quesDisplay.style.display = 'flex';
-    openScoreBtn.style.display = 'block';
-    closeScoreBtn.style.display = 'none'
-})
-
-// START MODAl
-window.addEventListener("load", () => {
-    mainJs.style.opacity = '0.5';
-    exitModal.style.display = 'none';
-});
-
-startBtn.addEventListener('click', () => {
-    mainJs.style.opacity = '1';
-    startModal.style.display = 'none';
-    setInterval(updateCounter, 1000);
-});
-
-// EXIT MODAL
-exitBtn.addEventListener('click', () => {
-    mainJs.style.opacity = '0.5';
-    exitModal.style.display = 'flex';
-})
-
-noBtn.addEventListener('click', () => {
-    mainJs.style.opacity = '1';
-    exitModal.style.display = 'none';
-})
-
-yesBtn.addEventListener('click', () => {
-    window.location.replace("./tq.html");
-})
 
 // QUESTIONS
 
-const questions = [
+let questions = [
     {
         flag: 'https://countryflagsapi.com/png/br',
         answer : [
@@ -176,3 +94,106 @@ const questions = [
     }
 
 ]
+
+const mainJs = document.querySelector('main');
+const scoreDisplay = document.querySelector('#score-display');
+const quesDisplay = document.querySelector('#ques-display');
+
+// QNA
+let flagEl = document.querySelector('.flag');
+const optA = document.querySelector('.a');
+const optB = document.querySelector('.b');
+const optC = document.querySelector('.c');
+const optD = document.querySelector('.d');
+let quesIndex;
+
+// timer elements
+const counterJs = document.querySelector('.counter');
+
+// modals
+const startModal = document.querySelector('.start-modal');
+const exitModal = document.querySelector('.exit-modal');
+
+// buttons
+const startBtn = document.querySelector('.start-btn');
+const exitBtn = document.querySelector('.exit-btn');
+const yesBtn = document.querySelector('.y-btn');
+const noBtn = document.querySelector('.n-btn');
+const openScoreBtn = document.querySelector('.open');
+const closeScoreBtn = document.querySelector('.close');
+const answerBtn = document.querySelectorAll('.answer-btn');
+
+// timer
+const startTime = 5;
+let totalTime = startTime * 60;
+
+updateCounter = () => {
+    const minute = Math.floor(totalTime / 60);
+    let second = totalTime % 60;
+
+    
+    counterJs.innerHTML = `0${minute} : ${second}`;
+    if(second < 10){
+        counterJs.innerHTML = `0${minute} : 0${second}`;
+    }
+
+    // if(minute === -1){
+    //     counterJs.innerHTML = `00 : 00`;
+    //     clearInterval(updateCounter);
+    //     window.location.replace('./tq.html');
+    // }
+
+    totalTime--;
+}
+
+// OPENING SCORE DISPLAY ON SMALLER SCREEN
+openScoreBtn.addEventListener('click', () => {
+    scoreDisplay.style.display = 'flex';
+    quesDisplay.style.display = 'none';
+    openScoreBtn.style.display = 'none';
+    closeScoreBtn.style.display = 'block'
+})
+closeScoreBtn.addEventListener('click', () => {
+    scoreDisplay.style.display = 'none';
+    quesDisplay.style.display = 'flex';
+    openScoreBtn.style.display = 'block';
+    closeScoreBtn.style.display = 'none'
+})
+
+// START MODAl
+window.addEventListener("load", () => {
+    mainJs.style.opacity = '0.5';
+    exitModal.style.display = 'none';
+});
+
+startBtn.addEventListener('click', () => {
+    mainJs.style.opacity = '1';
+    startModal.style.display = 'none';
+    setInterval(updateCounter, 1000);
+
+    quesIndex = Math.floor(Math.random() * 9);
+    setQues(quesIndex);
+});
+
+// EXIT MODAL
+exitBtn.addEventListener('click', () => {
+    mainJs.style.opacity = '0.5';
+    exitModal.style.display = 'flex';
+})
+
+noBtn.addEventListener('click', () => {
+    mainJs.style.opacity = '1';
+    exitModal.style.display = 'none';
+})
+
+yesBtn.addEventListener('click', () => {
+    window.location.replace("./tq.html");
+})
+
+const setQues = (e) => {
+    flagEl.src = questions[e].flag
+    optA.innerHTML = questions[e].answer[0].option;
+    optB.innerHTML = questions[e].answer[1].option;
+    optC.innerHTML = questions[e].answer[2].option;
+    optD.innerHTML = questions[e].answer[3].option;
+}
